8dee5f51a7e82287c0025654c90e66b89ad86545
Petitjean Théo <60598493+Theopetitjean@users.noreply.github.com>
2022-03-03 09:52:22 +0100

    Update loss.py

--- DenseF_R_Inv/lib/loss.py
+++ DenseF_R_Inv/lib/loss.py
@@ -12,8 +12,7 @@ from lib.knn.__init__ import KNearestNeighbor
 def loss_calculation(pred_r, pred_t, pred_c, target, model_points, idx, points, w, refine, num_point_mesh, sym_list):
     knn = KNearestNeighbor(1)
     bs, num_p, _ = pred_c.size()
-    print(f'bs is : {bs}, and num_p {num_p} \n ')
-
+    # print(f'bs is : {bs}, and num_p {num_p} \n ')
     pred_r = pred_r / (torch.norm(pred_r, dim=2).view(bs, num_p, 1))
 
 
@@ -28,9 +27,10 @@ def loss_calculation(pred_r, pred_t, pred_c, target, model_points, idx, points, 
                       (1.0 - 2.0*(pred_r[:, :, 1]**2 + pred_r[:, :, 2]**2)).view(bs, num_p, 1)), dim=2).contiguous().view(bs * num_p, 3, 3)
 
     print(f' the size of Pred R is : {pred_r.size()} and the size of base is {base.size()} \n ')
+
+
     ori_base = base
     base = base.contiguous().transpose(2, 1).contiguous()
-
     print(f'the model point size is : {model_points.size()} and the targt point size is {target.size()} \n')
 
     model_points = model_points.view(bs, 1, num_point_mesh, 3).repeat(1, num_p, 1, 1).view(bs * num_p, num_point_mesh, 3)
@@ -44,12 +44,13 @@ def loss_calculation(pred_r, pred_t, pred_c, target, model_points, idx, points, 
     pred_c = pred_c.contiguous().view(bs * num_p)
     print(f'the size of pred_t is : {pred_t.size()}, the size of points is : {points.size()} \n ')
 
-
     pred = torch.add(torch.bmm(model_points, base), points + pred_t)
     print(f'pred size is : {pred.size()} \n ')
+    sys.exit("\nERROR 106: Program EXIT~System INTERRUPT || Normal SHUTDOWN CODE STOPPED NORMALLY || CHECK Tpredicted format COMPATIBILITY\n ")
 
     if not refine:
         if idx[0].item() in sym_list:
+            print('\n cest ici que ca casse ? \n ')
             target = target[0].transpose(1, 0).contiguous().view(3, -1)
             pred = pred.permute(2, 0, 1).contiguous().view(3, -1)
             inds = knn(target.unsqueeze(0), pred.unsqueeze(0))
@@ -57,35 +58,13 @@ def loss_calculation(pred_r, pred_t, pred_c, target, model_points, idx, points, 
             target = target.view(3, bs * num_p, num_point_mesh).permute(1, 2, 0).contiguous()
             pred = pred.view(3, bs * num_p, num_point_mesh).permute(1, 2, 0).contiguous()
 
-
-
-    for i in model_points :
-        a = 0
-        b = a + 1
-        for j in target:
-            print(f'Inside (i a) there is : {i[a]}')
-            print(f'Inside (j a) there is : {j[a]}')
-            print(f'The size of i si {i.size()}, the size on j is : {j.size()}')
-            cij_model = ((i[a] + pred_t).transpose(1,0) * (i[b] + pred_t))
-            cij_Gt = ((j[a] + pred_t).transpose(1,0) * (j[b] + pred_t))
-            a += 1
-            b += 1
-            # print(f'the cij model is : {cij_model} and of size : {cij_model.size()}')
-            # print(f'the cij GT is : {cij_Gt} and of size : {cij_Gt.size()}')
-            break
-        break
-
-
-    print(f'The target size is : {target.size()}, and the pred size is : {pred.size()} \n ')
+    # print(f'The target size is : {target.size()}, and the pred size is : {pred.size()} \n ')
     dis = torch.mean(torch.norm((pred - target), dim=2), dim=1)
     loss = torch.mean((dis * pred_c - w * torch.log(pred_c)), dim=0)
-
-
 
     pred_c = pred_c.view(bs, num_p)
     how_max, which_max = torch.max(pred_c, 1)
     dis = dis.view(bs, num_p)
-
 
     t = ori_t[which_max[0]] + points[which_max[0]]
     points = points.view(1, bs * num_p, 3)
